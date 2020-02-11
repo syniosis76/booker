6afa9fbda1a1892530ea067094d2fe559cdb6316
Stacey Verner <stacey@verner.co.nz>
2020-02-11 21:03:13 +1300

    Setup for VS Code.

---
+++ requirements.bat
@@ -0,0 +1,4 @@
+python -m pip install --upgrade pip wheel setuptools virtualenv
+python -m pip install docutils pygments pypiwin32 kivy_deps.sdl2==0.1.* kivy_deps.glew==0.1.*
+python -m pip install kivy_deps.angle==0.1.*
+python -m pip install kivy==1.11.1
