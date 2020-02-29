42e9b45fd4b7547841b410686da846a915625d13
Stacey Verner <stacey@verner.co.nz>
2020-02-29 14:25:43 +1300

    Save to file.

--- models/data.py
+++ models/data.py
@@ -3,9 +3,13 @@ from models.person import Person
 from models.timeslot import Timeslot
 from models.booking import Booking
 from datetime import datetime
+import os.path
+import pickle
 
 class Data():    
     def __init__(self):
+        self.filename = 'data.data'
+
         self.products = []
         self.people = []
         self.timeslots = []
@@ -15,78 +19,35 @@ class Data():
         self.currentTimeslot = None
         self.currentProduct = None 
 
-        self.loadData() 
-
-    def loadProducts(self):
-        product = Product()
-        product.name = 'Hitman XS'
-        self.products.append(product)
-
-        product = Product()
-        product.name = 'Hitman S'
-        self.products.append(product)
-
-        product = Product()
-        product.name = 'Hitman M'
-        self.products.append(product)
-
-        product = Product()
-        product.name = 'Hitman L'
-        self.products.append(product)              
-
-    def loadPeople(self):
-        person = Person()
-        person.name = 'Stacey Verner'
-        person.nick_name = 'Stacey V'
-        person.email_address = 'stacey@verner.co.nz'
-        person.mobile_number = '021 074 7965'        
-        self.people.append(person)
-
-        person = Person()
-        person.name = 'Anna Verner'
-        person.nick_name = 'Anna V'
-        person.email_address = 'anna@verner.co.nz'
-        person.mobile_number = '021 298 4390'        
-        self.people.append(person)
-
-    def loadTimeslots(self):
-        self.timeslots.clear()
-        
-        self.timeslots.append(Timeslot('08:00'))
-        self.timeslots.append(Timeslot('08:30'))
-        self.timeslots.append(Timeslot('09:00'))
-        self.timeslots.append(Timeslot('09:30'))
-        self.timeslots.append(Timeslot('10:00'))
-        self.timeslots.append(Timeslot('10:30'))
-        self.timeslots.append(Timeslot('11:00'))
-        self.timeslots.append(Timeslot('11:30'))
-        self.timeslots.append(Timeslot('12:00'))
-        self.timeslots.append(Timeslot('12:30'))
-        self.timeslots.append(Timeslot('13:00'))
-        self.timeslots.append(Timeslot('13:30'))
-        self.timeslots.append(Timeslot('14:00'))
-        self.timeslots.append(Timeslot('14:30'))
-        self.timeslots.append(Timeslot('15:00'))
-        self.timeslots.append(Timeslot('15:30'))
-        self.timeslots.append(Timeslot('16:00'))
-        self.timeslots.append(Timeslot('16:30'))
-
-    def loadBookings(self):
-        self.bookings.clear()
-
-        booking = Booking()
-        booking.product = 'Hitman S'
-        booking.timeslot = '08:00'
-        booking.email_address = 'anna@verner.co.nz'
-        self.bookings.append(booking)        
+        self.loadData()     
 
     def loadData(self):
-        self.loadProducts()
-        self.loadPeople()
-        self.loadTimeslots()
-        self.loadBookings()
+        self.load()                    
+        self.defaultProducts()
+        self.defaultPeople()            
+        self.defaultTimeslots()
+        self.defaultBookings()    
 
-        self.updateVersion()
+        self.updateVersion()    
+
+    def load(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, 'rb') as filehandle:            
+                data = pickle.load(filehandle)
+                self.products = data['products']
+                self.people = data['people']
+                self.timeslots = data['timeslots']
+                self.bookings = data['bookings']
+
+    def save(self):
+        data = {}
+        data['products'] = self.products
+        data['people'] = self.people
+        data['timeslots'] = self.timeslots
+        data['bookings'] = self.bookings
+
+        with open(self.filename, 'wb') as filehandle:            
+            pickle.dump(data, filehandle)
 
     def getBooking(self, product, timeslot):
         booking = next((booking for booking in self.bookings if booking.product == product and booking.timeslot == timeslot), None)
@@ -106,6 +67,8 @@ class Data():
         booking.email_address = email_address
         self.bookings.append(booking)
 
+        self.save()
+
     def setProducts(self, products):
       self.products.clear()
 
@@ -118,6 +81,70 @@ class Data():
 
     def updateVersion(self):
       self.version += 1
+      self.save()
 
     def hasUpdated(self, version):
       return self.version > version
+
+    def defaultProducts(self):
+        if len(self.products) == 0:        
+            product = Product()
+            product.name = 'Hitman XS'
+            self.products.append(product)
+
+            product = Product()
+            product.name = 'Hitman S'
+            self.products.append(product)
+
+            product = Product()
+            product.name = 'Hitman M'
+            self.products.append(product)
+
+            product = Product()
+            product.name = 'Hitman L'
+            self.products.append(product)              
+
+    def defaultPeople(self):
+        if len(self.people) == 0: 
+            person = Person()
+            person.name = 'Stacey Verner'
+            person.nick_name = 'Stacey V'
+            person.email_address = 'stacey@verner.co.nz'
+            person.mobile_number = '021 074 7965'        
+            self.people.append(person)
+
+            person = Person()
+            person.name = 'Anna Verner'
+            person.nick_name = 'Anna V'
+            person.email_address = 'anna@verner.co.nz'
+            person.mobile_number = '021 298 4390'        
+            self.people.append(person)
+
+    def defaultTimeslots(self):
+        if len(self.timeslots) == 0:                         
+            self.timeslots.append(Timeslot('08:00'))
+            self.timeslots.append(Timeslot('08:30'))
+            self.timeslots.append(Timeslot('09:00'))
+            self.timeslots.append(Timeslot('09:30'))
+            self.timeslots.append(Timeslot('10:00'))
+            self.timeslots.append(Timeslot('10:30'))
+            self.timeslots.append(Timeslot('11:00'))
+            self.timeslots.append(Timeslot('11:30'))
+            self.timeslots.append(Timeslot('12:00'))
+            self.timeslots.append(Timeslot('12:30'))
+            self.timeslots.append(Timeslot('13:00'))
+            self.timeslots.append(Timeslot('13:30'))
+            self.timeslots.append(Timeslot('14:00'))
+            self.timeslots.append(Timeslot('14:30'))
+            self.timeslots.append(Timeslot('15:00'))
+            self.timeslots.append(Timeslot('15:30'))
+            self.timeslots.append(Timeslot('16:00'))
+            self.timeslots.append(Timeslot('16:30'))
+
+    def defaultBookings(self):
+        if len(self.bookings) == 0:
+            booking = Booking()
+            booking.product = 'Hitman S'
+            booking.timeslot = '08:00'
+            booking.email_address = 'anna@verner.co.nz'
+            self.bookings.append(booking)        
