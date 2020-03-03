fa84048925224732fbea4778a3cc3b14e4bc178a
Stacey Verner <stacey@verner.co.nz>
2020-03-03 21:14:05 +1300

    Improved layout.

--- views/bookingListView.py
+++ views/bookingListView.py
@@ -9,6 +9,9 @@ from kivy.uix.floatlayout import FloatLayout
 
 Builder.load_file('views/bookingListView.kv')
 
+def sizeCallback(obj, value):
+    obj.text_size = (value[0] - 30, 20)
+
 class BookingListView(Screen):
     def __init__(self, **kwargs):        
         super(BookingListView, self).__init__(**kwargs)                   
@@ -29,10 +32,39 @@ class BookingListView(Screen):
 
         rowHeight = 40
         
-        for product in app.data.products:
+        for productIndex in range(len(app.data.products)):
+            product = app.data.products[productIndex]
+            
             grid = GridLayout()
-            grid.cols = 1                       
-            grid.add_widget(Label(text=product.name, size_hint_y=0.075))            
+            grid.cols = 1
+
+            headerGrid = GridLayout()
+            headerGrid.cols = 3
+            headerGrid.size_hint_y=0.1
+
+            button = Button()
+            button.background_normal = ''
+            button.background_color = [0, 0, 0, 1]
+            button.bind(on_press=self.previousButtonClick)
+            if productIndex >= 1:
+                button.text = '<  ' + app.data.products[productIndex - 1].name                                   
+            headerGrid.add_widget(button)            
+
+            label = Label()
+            label.text = product.name            
+            label.font_size = 24
+            label.bold = True
+            headerGrid.add_widget(label)
+
+            button = Button()
+            button.background_normal = ''
+            button.background_color = [0, 0, 0, 1]
+            button.bind(on_press=self.nextButtonClick)
+            if productIndex < len(app.data.products) - 1:
+                button.text = app.data.products[productIndex + 1].name + '  >'                                  
+            headerGrid.add_widget(button)
+
+            grid.add_widget(headerGrid)
 
             scrollView = ScrollView()
             scrollView.do_scroll_x = False
@@ -46,9 +78,13 @@ class BookingListView(Screen):
                 button = Button()
                 button.booker_product = product.name
                 button.booker_timeslot = timeslot.time
-                button.text = timeslot.time              
-                button.bind(on_press = self.buttonClick)                
-                innerGrid.add_widget(button)
+                button.text = timeslot.time                
+                button.background_color = [0.0, 0.435, 0.698, 1.0]
+                button.markup = True              
+                button.halign = 'left'                                                
+                button.bind(size = sizeCallback)
+                button.bind(on_press = self.buttonClick)                                
+                innerGrid.add_widget(button)                
 
             innerGrid.height = rowHeight * len(app.data.timeslots)
             scrollView.add_widget(innerGrid)
@@ -61,12 +97,14 @@ class BookingListView(Screen):
         app = App.get_running_app()
         
         for widget in widgets.children:
-            if isinstance(widget, Button):
+            if isinstance(widget, Button) and hasattr(widget, 'booker_product'):
                 person = app.data.getBooking(widget.booker_product, widget.booker_timeslot)
                 if person:
-                    widget.text = widget.booker_timeslot + ' ' + person.name
+                    widget.text = widget.booker_timeslot + '    [b]' + person.name + '[/b]'
+                    widget.background_color = [0.137, 0.658, 0.949, 1.0]
                 else:
                     widget.text = widget.booker_timeslot
+                    widget.background_color = [0.0, 0.435, 0.698, 1.0]
             else:
                 self.listBookings(widget)
 
@@ -78,6 +116,14 @@ class BookingListView(Screen):
 
         self.manager.transition.direction = 'left'
         self.manager.current = 'people'
+    
+    def nextButtonClick(self, instance):
+        carousel = self.ids.bookings
+        carousel.load_next()
+
+    def previousButtonClick(self, instance):
+        carousel = self.ids.bookings
+        carousel.load_previous()
     
     def productsButtonClick(self, instance):
         print('Booking button <%s> clicked.' % instance.text)
